1b1a40038a5a8350d4e840c4e400a634c1cb8d15
hekoshi <47317489+hekoshi3@users.noreply.github.com>
2025-04-02 01:09:16 +0700

    interactive_box_scripts
    + InteractiveBox
    + InteractiveRaycast
    + CreateScript prefab
    + intbox_cube prefab

--- Assets/Prac6/Scripts/hksi/InteractiveBox.cs
+++ Assets/Prac6/Scripts/hksi/InteractiveBox.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class InteractiveBox : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private InteractiveBox next;
+
+    public void AddNext(InteractiveBox box)
     {
-        
+        next = box;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (next != null)
+        {
+            Vector3 start = transform.position;
+            Vector3 end = next.transform.position;
+
+            Debug.DrawLine(start, end, Color.green, 0.1f);
+
+            Ray ray = new Ray(start, (end - start).normalized);
+            if (Physics.Raycast(ray, out RaycastHit hit, Vector3.Distance(start, end)))
+            {
+                ObstacleItem obstacle = hit.collider.GetComponent<ObstacleItem>();
+                if (obstacle != null)
+                {
+                    obstacle.GetDamage(Time.deltaTime);
+                }
+            }
+        }
     }
 }
